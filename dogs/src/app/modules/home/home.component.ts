1c78e3ebcc784d9a1543da0cdf283c81cdeadadc
Maria Fernanda Lanconi <maria.lanconi@engdb.com.br>
2025-09-23 20:07:00 -0300

    FEAT: Home com implementação do modal

--- dogs/src/app/modules/home/home.component.ts
+++ dogs/src/app/modules/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DogService } from '../../../services/dog.service';
-import { DogsBreed } from '../../../shared/model/dogsBreed.model';
+import { DogsBreed, DogsBreedDetails } from '../../../shared/model/dogsBreed.model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalDogsInfoComponent } from '../../../components/modal-dogs-info/modal-dogs-info.component';
+import { ModalFormComponent } from '../../modal-form/modal-form.component';
 
 @Component({
   selector: 'app-home',
@@ -14,20 +17,25 @@ import { FormsModule } from '@angular/forms';
 export class HomeComponent implements OnInit {
   breeds: DogsBreed[] = [];
   breedSelected: DogsBreed | null = null;
+  breedDetailed: DogsBreedDetails | null = null; 
   breedFiltered: DogsBreed[] = [];
+  constructor(
+    private dogService: DogService,
+    private dialog: MatDialog
+  ) { }
 
-  constructor(private dogService: DogService) { }
-
-  public get breedList() : DogsBreed[] {
-    return this.breedSelected? this.breedFiltered : this.breeds;
-  }
-  
   ngOnInit() {
     this.dogService.getDogsBreed().subscribe({
       next: (data: DogsBreed[]) => {
         this.breeds = data;
+        console.log(this.breeds);
+        
       }
     });
+  }
+
+  public get breedList(): DogsBreed[] {
+    return this.breedSelected ? this.breedFiltered : this.breeds;
   }
 
   onBreedChange(breed: DogsBreed | null) {
@@ -38,22 +46,30 @@ export class HomeComponent implements OnInit {
 
     this.dogService.searchDogsByBreed(breed?.name).subscribe({
       next: (data: any) => {
-        this.breedFiltered = data;        
+        this.breedFiltered = data;
       }
     });
   }
 
-  getDogDetails(breedId: number) {
-    this.dogService.getBreedDetails(breedId).subscribe({
-      next: (data: any) => {
-        console.log(data);
+  getDogDetails(breed:DogsBreed) {
+    this.dogService.getBreedDetails(breed.id).subscribe({
+      next: (data: DogsBreedDetails) => {
+        this.breedDetailed = data;
+        this.breedDetailed.reference_image_id = breed.image.url;
       }
+    });
+  }
+
+  openDogInfo(breed: DogsBreed) {
+    this.getDogDetails(breed);
+    const dialogRef = this.dialog.open(ModalDogsInfoComponent, {
+      width: '400px',
+      data: this.breedDetailed
     });
   }
 
   deleteBreed(breed: DogsBreed) {
     const indexOfbreed = this.breeds.indexOf(breed);
-    this.breeds = this.breeds.splice(indexOfbreed, 1);
+    this.breeds.splice(indexOfbreed, 1);
   }
 }
-  
